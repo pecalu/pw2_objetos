3a6e443cb20ffd92f8882df2bdfef88a31b2d0d1
lucape <lucaspedrogit@gamil.com>
2023-03-07 08:53:53 -0300

    criacao de objetos

--- src/anime.java
+++ src/anime.java
@@ -3,11 +3,11 @@ import java.time.LocalDate;
 public class anime {
     public int id;
     public String nome;
-    public LocalDate data_lançamento;
-    public int classe_indicativa;
+    public LocalDate lançamento;
+    public int classeIndicativa;
     public String genero;
     public String autor;
-    public int quantidadeEpisodios;
+    public int episodios;
     public int temporadas;
 
 }
--- src/animeTeste.java
+++ src/animeTeste.java
@@ -3,13 +3,18 @@ public class animeTeste {
         //classe nomeObjeto = new classe ();
         anime meuAnime = new anime();
         meuAnime.nome = "one peace";
-        meuAnime.quantidadeEpisodios = 1033;
+        meuAnime.episodios = 1053;
         meuAnime.temporadas = 20;
 
         anime meuAnimePreferido = new anime();
         meuAnimePreferido.nome = "naruto";
-        meuAnimePreferido.quantidadeEpisodios = 720;
+        meuAnimePreferido.episodios = 720;
         meuAnimePreferido.temporadas = 20;
+
+        anime piorAnime = new anime();
+        piorAnime.nome = "death note";
+        piorAnime.episodios = 52;
+        piorAnime.temporadas = 2;
 
     }
 }
