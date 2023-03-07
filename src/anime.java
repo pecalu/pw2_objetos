8eb193b87cf23d823ec33d77c9b5b8501665bc4e
lucape <lucaspedrogit@gamil.com>
2023-03-07 08:39:07 -0300

    first commit

---
+++ src/anime.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate:
+
+public class anime {
+    public int id;
+    public String nome;
+    public LocalDate data_lançamento;
+    public int classe_indicativa;
+    public String genero;
+    public String autor;
+    public int qtd_episodio;
+    public int temporada;
+
+}
---
+++ src/animeTeste.java
@@ -0,0 +1,11 @@
+public class animeTeste {
+    public static void main (String[] args){
+        //classe nomeObjeto = new classe ();
+        anime meuAnime = new anime();
+        meuAnime.nome = "death note";
+
+        anime meuAnimePreferido = new anime();
+        meuAnimePreferido.nome = "dragon ball"
+
+    }
+}
